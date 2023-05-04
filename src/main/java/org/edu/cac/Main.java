bcc0e1b6eacdea4b039be50946e8a2e24dcad9b2
Jeboy <tovilajesus@gmail.com>
2023-05-04 14:35:04 -0300

    Programación funcional

--- src/main/java/org/edu/cac/Main.java
+++ src/main/java/org/edu/cac/Main.java
@@ -5,11 +5,13 @@ import org.edu.cac.enums.Posicion;
 
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-
+        /*
         Map<String, Integer> posiciones = new HashMap<>();
         posiciones.put("LFP", 2);
         posiciones.put("Copa Sudamericana", 1);
@@ -34,7 +36,7 @@ public class Main {
             Integer key = (Integer) it.next();
             System.out.println("Clave: " + key + " ->Valor: " + equipo1.getJugadores().get(key));
         }
-
+*/
         // INTERFACES Y CLASES ABSTRACTAS
 
         // INTERFAZ: Contrato en donde se especifican
@@ -52,5 +54,41 @@ public class Main {
         // Para poder ser una clase abstracta, al menos uno de sus métodos tienes que ser abstracto
         // es decir, que no tenga cuerpo (solo la firma)
         // Puede tener métodos no abstractos (comunes)
+
+
+
+
+        // Programación Funcional
+
+        // Interfaces Funcionales: Son interfaces con 1 único método abstracto
+        // @FuntionalInterface
+        // public void operacion(Integer x, Integer y);
+
+        // Expresiones lambda: Representan una operación -> (flecha) / método implementado
+        // ej: s -> System.out.println(s)
+
+        // La flecha lambda se realiza con el signo menos seguido del signo mayor que - >
+
+
+        // Referencia a métodos: Utilizando el :: (operador de ambito), para hacer referencia a una función
+        // de una clase específica(System.out::println)
+
+        // Streams: Encapsulamiento de colecciones para trabajarlas mediante la concatenación de diferentes
+        // métodos y filtros (que no mantienen estado), es decir que no modifican al original.
+
+        // Ej:
+
+        List<String> nombres = List.of("Cristian", "Mariana", "Carlos", "Luciana");
+
+        List<String> nombrecConC = nombres.stream()
+                .filter(nombre -> nombre.charAt(0) == 'C')
+                .collect(Collectors.toList());
+
+        System.out.println("Lista original:");
+        nombres.forEach(System.out::println);
+
+        System.out.println("Lista filtrada:");
+        nombrecConC.forEach(System.out::println);
+
     }
 }
